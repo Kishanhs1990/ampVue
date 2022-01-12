ebed812d18e16bfb6987155c950a5168df26bd76
H S <kishan.h.s@accenture.com>
2022-01-12 23:35:50 +0530

    testing deployment

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div id="app">
     <div>
       <b-navbar toggleable="lg" type="dark" variant="success">
-        <b-navbar-brand href="#">ampVue</b-navbar-brand>
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav class="mx-auto">
